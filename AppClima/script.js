d02bddd87a8f547d2a2d36e7062d462b3f3b21ac
Federico Suarez <federicosuarez.seosem@gmail.com>
2024-09-19 17:03:10 -0500

    Se creo la funcion que muestra todos los datos del clima

--- AppClima/script.js
+++ AppClima/script.js
@@ -13,5 +13,37 @@ document.getElementById('searchButton').addEventListener('click',()=>{
 function fetchClima(city){
     fetch(`${urlBase}?q=${city}&appid=${API_KEY}&lang=es`)
     .then(data => data.json())
-    .then(data => console.log(data));
+    .then(data => mostrarDatosClima(data));
+}
+function mostrarDatosClima(data){
+    const divResponseData = document.getElementById('responseData');
+    divResponseData.innerHTML = '';
+    //obtenemos el nombre de la ciudad 
+    const cityName = data.name;
+    // Obtenemos el nombre del pais
+    const countryName = data.sys.country;
+    // Obtenemos la temperatura
+    const temp = data.main.temp;
+    // Obtenemos la humedad
+    const humidity = data.main.humidity;
+    // Obtenemos la descripcion
+    const description = data.weather[0].description;
+    // Obtenemos el icono del clima
+    const icon = data.weather[0].icon;
+
+    const cityInfo = document.createElement('h2');
+    cityInfo.textContent = `${cityName},${countryName}`;
+    const tempInfo = document.createElement('p');
+    tempInfo.textContent = `La temperatura es: ${Math.floor(temp-diffKelvin)}°C`;
+    const humidityInfo = document.createElement('p');
+    humidityInfo.textContent = `La humedad es del ${humidity}%`;
+    const iconInfo = document.createElement('img');
+    iconInfo.src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+    const descriptionInfo = document.createElement('p');
+    descriptionInfo.textContent = `La descripción meteorológica es: ${description}`;
+    divResponseData.appendChild(cityInfo);
+    divResponseData.appendChild(tempInfo);
+    divResponseData.appendChild(humidityInfo);
+    divResponseData.appendChild(iconInfo);
+    divResponseData.appendChild(descriptionInfo);
 }
