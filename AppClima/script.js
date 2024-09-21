2fb21f4dacdd714339f993b06caff54cf6081cab
Federico Suarez <federicosuarez.seosem@gmail.com>
2024-09-20 19:58:58 -0500

    Agrego la descripcion del proyecto

--- AppClima/script.js
+++ AppClima/script.js
@@ -1,5 +1,5 @@
 const urlBase = `https://api.openweathermap.org/data/2.5/weather`;
-const API_KEY = 'Colocar aqui su API KEY';
+const API_KEY = 'API KEY';
 const diffKelvin = 273.15;
 
 document.getElementById('searchButton').addEventListener('click',()=>{
